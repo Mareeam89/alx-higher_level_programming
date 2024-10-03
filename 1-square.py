24cc7c25fce1845e26cf395da7ea71211e70b265
Mareeam89 <162319111+Mareeam89@users.noreply.github.com>
2024-10-03 02:21:36 +0100

    Add files via upload

---
+++ 1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Representing a square"""
+
+
+class Square():
+    """A square object representation"""
+
+    def __init__(self, size):
+        """ initialize the newly created square
+        where: size equates the size of the square
+        """
+        self.__size = size
