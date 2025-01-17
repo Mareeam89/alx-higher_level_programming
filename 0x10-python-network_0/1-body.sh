4bac61a997a70305fded19527e76c6bec563353b
Mareeam89 <mareeamraj@gmail.com>
2025-01-17 15:08:34 +0100

    display body of 200 status code response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# Send a GET request and display the body of the response only if the status code is 200
-response=$(curl -s -w "%{http_code}" -o response.txt "$1")
-if [ "$response" -eq 200 ]; then
-  cat response.txt
-fi
-
+# send a GET request to an URL with curl, and display the body of the response
+curl -sL "$1"
